69821e72632351dd819de44ef5016e81a78156e2
Shokrof <mostafa.shokrof@gmail.com>
2013-12-20 16:15:48 +0200

    base code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ostaz::Application.config.secret_token = 'fbf2042db97bc70d661b68e86ff44785e2ca8221a1b51f48e7da01cf98874679ecc392ca24c9f5288afd6a8e83ff3eb97feaf547b1a2a4f9e5941f68099ee40d'
